e1bc3448371ef2f88d22e36afbc0c598347965c9
Faringardo <58985100+Faringardo@users.noreply.github.com>
2025-03-21 18:31:01 +0300

    Initializing an array of characters

---
+++ src/Array/arrayOfCharacters.java
@@ -0,0 +1,14 @@
+package Array;
+
+import java.util.Arrays;
+
+public class arrayOfCharacters {
+    public static void main(String[] args) {
+        char[] characters = new char[4];
+        characters[0] = 'a';
+        characters[1] = 'z';
+        characters[2] = 'e';
+        characters[3] = 'd';
+        System.out.println(Arrays.toString(characters));
+    }
+}
